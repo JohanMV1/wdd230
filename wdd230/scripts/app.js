44edfd0ab5d320ec959117329c0536d65020ea1b
JohanMV1 <114034652+JohanMV1@users.noreply.github.com>
2023-01-06 17:12:54 -0800

    Add files via upload

---
+++ wdd230/scripts/app.js
@@ -0,0 +1,10 @@
+const h1 =document.querySelector('h1');
+const copyright = document.querySelector('#copyright');
+
+h1.textContent = 'Brother Johan Villarreal';
+
+copyright.textContent = `©️ ${new Date().getFullYear()}`;
+let quantity =document.querySelector('#q').value;
+
+const h2 = document.querySelector('h2');
+console.log(document.lastModified);
